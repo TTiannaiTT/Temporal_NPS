6f25dfa0f1cf29fda7cf4143d1e9337599a59030
TTiannaiTT <546893779@qq.com>
2024-05-01 01:03:44 +0800

    update 5.1

--- Temporal_Sequence/utils/feature.py
+++ Temporal_Sequence/utils/feature.py
@@ -2,15 +2,28 @@ import pandas as pd
 from scipy import stats
 import numpy as np
 from imblearn.under_sampling import RandomUnderSampler
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.preprocessing import StandardScaler
+
 # 读取训练集
 train_data = pd.read_csv('/home/tyk/EventAug/Temporal_Sequence/dataset/train.csv')
 
-# 假设特征列名为'features'，目标列名为'target'
 X_train = train_data.iloc[:, 3:]
 y_train = train_data.iloc[:, 2]
 
+# 数据预处理
+X_train_ = train_data.iloc[:,2:]
+
+# 计算每列的均值
+mean_values = X_train_.mean()
+# 使用每列的均值填充NaN
+X_train_ = X_train_.fillna(mean_values)
+# print(X_train_['LABEL'])
+
 # 使用Z分数检测异常值
-def detect_outliers_zscore(data, label, threshold=3):
+def detect_outliers_zscore(data, threshold=3):
     return data[(np.abs(stats.zscore(data)) > threshold).any(axis=1)]
 
 # 使用IQR方法检测异常值
@@ -30,12 +43,36 @@ def under_sample(X,y):
     X_resampled, y_resampled = rus.fit_resample(X, y)
     return X_resampled, y_resampled
 
-# 检测并删除异常值
-print(X_train.shape)
-train_data_cleaned1,_ = detect_outliers_iqr(X_train,y_train)
-print(train_data_cleaned1.shape)
-print(_.shape)
-x_,y_ = under_sample(X_train.values,y_train.values)
-print(x_.shape)
-print(y_.shape)
+def get_main_feature():
+    # 方差阈值筛选
+    selector = VarianceThreshold(threshold=0)  # 移除方差为0的特征
+    df_variance_filtered = selector.fit_transform(X_train_)
+    # 相关性筛选示例（以卡方检验为例，适用于分类特征）
+    bestfeatures = SelectKBest(score_func=chi2, k=10)
+    print(X_train_['LABEL'])
+    fit = bestfeatures.fit(df_variance_filtered, X_train_['LABEL'])
+    dfscores = pd.DataFrame(fit.scores_)
+    dfcolumns = pd.DataFrame(X_train_.columns)
+    featureScores = pd.concat([dfcolumns, dfscores], axis=1)
+    featureScores.columns = ['Feature', 'Score']
+    print(featureScores.nlargest(10, 'Score'))  # 输出得分最高的10个特征
+    print(type(featureScores)) #DataFrame
+    print(featureScores['Feature'])
+    # 注意这里是移除了前两列之后的列数，和真实的比要前2
 
+    # # 计算相关系数矩阵
+    # corr_matrix = X_train_.corr()
+    # # 选择与目标变量相关性高于某一阈值的特征
+    # print(corr_matrix["LABEL"].nlargest(10, "LABEL").index)
+    index = [1113,659,854,984,1761,672,1775,19,1774,1312]
+    print('main feature index: ',index)
+    return index
+# 检测并删除异常值
+# print(X_train.shape)
+# train_data_cleaned1,_ = detect_outliers_iqr(X_train,y_train)
+# print(train_data_cleaned1.shape)
+# print(_.shape)
+# x_,y_ = under_sample(X_train.values,y_train.values)
+# print(x_.shape)
+# print(y_.shape)
+get_main_feature()
